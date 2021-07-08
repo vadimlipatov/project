f0d7ce925fef723864458d962243d5ac40b59a4d
Leon <leon@mail.ru>
2021-07-08 21:56:05 +0400

    personal movie 1.0

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 "use strict";
-let answers = [];
 
-answers[0] = prompt('Name', '');
-answers[1] = prompt('Surname', '');
-answers[2] = prompt('Age', '');
+let numberOfFilms = +prompt("Сколько фотльмов вы уе посмотрели?", "");
+const movies = {};
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : {},
+    privat : false,
+};
+
+let q1 = prompt("один из просоирелнных последниз фильмов?", "");
+let a1 = prompt("на сколько оцените его?", "");
+movies[q1] = a1;
+let q2 = prompt("один из просоирелнных последниз фильмов?", "");
+let a2 = prompt("на сколько оцените его?", "");
+movies[q2] = a2;
+personalMovieDB.movies = movies;
+
+// console.log(movies);
+console.log(personalMovieDB);
